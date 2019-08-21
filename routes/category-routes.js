a09e24087f458269f6de7a0f556a13eed75b826b
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-08-21 10:16:13 -0700

    code review notes

--- routes/category-routes.js
+++ routes/category-routes.js
@@ -7,18 +7,18 @@ const router = express.Router();
 
 // Route to Get All Categories
 router.get('/categories', (req, res, next) => {
-    let count = db.length;
-    let results = db;
-    res.json({ count, results });
+  let count = db.length;
+  let results = db;
+  res.json({ count, results });
 });
 
 // Route to Create a Category
 router.post('/categories', isValid, (req, res, next) => {
-    let record = req.body;
-    console.log('I am app.post /categories');
-    record.id = Math.random();
-    db.push(record);
-    res.json(record);
+  let record = req.body;
+  console.log('I am app.post /categories');
+  record.id = Math.random();
+  db.push(record);
+  res.json(record);
 });
 
 module.exports = router; 
